4de332c2e3fd71bad1461e40da1dcd6dc66b8ff7
Alejandro Baez <code@baez.nyc>
2016-04-13 17:02:38 -0400

    elm: initial setup

---
+++ elm.lua
@@ -0,0 +1,39 @@
+--- lexer for [Elm](http://elm-lang.org/docs/syntax).
+-- Used haskell as reference.
+-- @author [Alejandro Baez](https://keybase.io/baez)
+-- @copyright 2016
+-- @license MIT (see LICENSE)
+-- @module elm
+
+local l = require("lexer")
+local token, word_match = l.token, l.word_match
+local P, R, S = lpeg.P, lpeg.R, lpeg.S
+
+local M = {_NAME = 'elm'}
+
+-- Whitespace.
+local ws = token(l.WHITESPACE, l.space^1)
+
+-- Comments.
+local line_comment = '--' * l.nonnewline_esc^0
+local block_comment = '{-' * (l.any - '-}')^0 * P('-}')^-1
+local comment = token(l.COMMENT, line_comment + block_comment)
+
+-- Strings.
+local string = token(l.STRING, l.delimited_range('"'))
+
+-- Chars.
+local char = token(l.STRING, "'" * l.alpha * "'")
+
+-- Numbers.
+local number = token(l.NUMBER, l.float + l.integer)
+
+-- Keywords.
+local keyword = token(l.KEYWORD, word_match{
+  'as',         'alias',      'and',      'case',     'else',
+  'exposing',   'if',         'in',       'infixr',   'import',
+  'let',        'module',     'not',      'of',       'or',
+  'then',       'type',       'var',      'where'
+})
+
+local alias = token("alias", l.upper^-1 * l.word^1)
