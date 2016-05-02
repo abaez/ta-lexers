11f6f0b64d1282ce4793758234fbc64c0b6bf46f
Alejandro Baez <code@baez.nyc>
2016-05-02 16:48:55 -0400

    moved spec to rpmspec

--- rpmspec.lua
+++ rpmspec.lua
@@ -2,13 +2,13 @@
 -- @author [Alejandro Baez](https://keybase.io/baez)
 -- copyright 2016
 -- @license MIT (see LICENSE)
--- @module spec
+-- @module rpmspec
 
 local l = require("lexer")
 local token, word_match = l.token, l.word_match
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
-local M = {_NAME = 'spec'}
+local M = {_NAME = 'rpmspec'}
 
 -- Whitespace
 local ws = token(l.WHITESPACE,  l.space^1)
