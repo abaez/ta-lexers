4dd89a2301da241d103d8a8b2284026fb811c5aa
Alejandro Baez <code@baez.nyc>
2016-05-07 18:08:25 -0400

    solidity: intro types

--- solidity.lua
+++ solidity.lua
@@ -18,19 +18,33 @@ local future_key = word_match{
   'try',          'type',         'typeof',     'using',
 }
 
+-- default keys
 local default_key = word_match{
   'anonymous',    'assembly',     'break',      'constant',     'continue',
-  'contract',     'default',      'do',         'else',         'enum',
-  'event',        'external',     'for',        'function',     'if',
-  'indexed',      'internal',     'import',     'import',       'is',
-  'library',      'mapping',      'memory',     'modifier',     'new',
-  'public',       'private',      'return',     'returns',      'storage',
-  'struct',       'throw',        'var',        'while'
+  'contract',     'default',      'delete'      'do',           'else',
+  'enum',         'event',        'external',   'for',          'function',
+  'if',           'indexed',      'internal',   'import',       'import',
+  'is',           'library',      'mapping',    'memory',       'modifier',
+  'new',          'public',       'private',    'return',       'returns',
+  'storage',      'struct',       'throw',      'var',          'while'
 }
 
 local keyword = token(l.KEYWORD, default_key + future_key)
 
+-- literal types
+local literals = token('literals', word_match{
+  'null',       'true',           'false',      'string',       'memory',
+  'storage',
+})
 
+-- dynamic types
+local dynamic = word_match{
+  'bytes',        'int',          'uint'
+}
+
+
+
+local type = token(l.TYPE, literals + dynamic)
 
 local operator = token(l.OPERATOR, S'()[]{}:;.?=<>|^&-+*/%,!~')
 
